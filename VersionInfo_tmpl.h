5ccd8589dc8585d397e75a05d8e9b87016825d12
eugen.kremer <eugen.kremer@139752b1-5060-c874-eff8-825b70bf1f57>
2013-12-28 09:51:13 +0000

    update VERSION

--- VersionInfo_tmpl.h
+++ VersionInfo_tmpl.h
@@ -16,7 +16,7 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
-#define VERSION_MA 1
+#define VERSION_MA 2
 #define VERSION_MI 0 
 
 // increment revision in case of modification
