12f2f66eec2875e2c3b8d458fc2dbe181304fcd1
eugen.kremer <eugen.kremer@139752b1-5060-c874-eff8-825b70bf1f57>
2014-03-24 21:42:41 +0000

    Improvements in debugging stuff 1. fix problem of not provided exceptions in case of developer tools are available 2. tried to solve syntax highlighting in VS via Scripting Engine, did not work

--- common/ActiveScriptSiteDebug.cpp
+++ common/ActiveScriptSiteDebug.cpp
@@ -17,12 +17,72 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 #include "ActiveScriptSiteDebug.h"
 #include <strsafe.h>
+#include <shlwapi.h>
+#include "SysStr.h"
+
+class ActiveScriptSiteDebugExImpl : public CComBase<IActiveScriptSiteDebugEx>{
+public:
+	virtual HRESULT STDMETHODCALLTYPE OnCanNotJITScriptErrorDebug(IActiveScriptErrorDebug *pErrorDebug, BOOL *pfCallOnScriptErrorWhenContinuing){
+		*pfCallOnScriptErrorWhenContinuing = true;
+		return S_OK;
+	}
+};
+
+class DebugDocumentHostImpl : public CComBase<IDebugDocumentHost>{
+private:
+	SysStr m_FilePath;
+	LocRef<IActiveScriptDebug, true> m_ActiveScriptDebug;
+public:
+	DebugDocumentHostImpl(const BSTR filePath, IActiveScriptDebug* activeScriptDebug):m_FilePath(filePath), m_ActiveScriptDebug(activeScriptDebug){
+	}
+	virtual HRESULT STDMETHODCALLTYPE GetDeferredText( 
+		/* [in] */ DWORD dwTextStartCookie,
+		/* [size_is][length_is][out][in] */ __RPC__inout_ecount_part(cMaxChars, *pcNumChars) WCHAR *pcharText,
+		/* [size_is][length_is][out][in] */ __RPC__inout_ecount_part(cMaxChars, *pcNumChars) SOURCE_TEXT_ATTR *pstaTextAttr,
+		/* [out][in] */ __RPC__inout ULONG *pcNumChars,
+		/* [in] */ ULONG cMaxChars){
+		return E_NOTIMPL;
+	}
+
+	virtual HRESULT STDMETHODCALLTYPE GetScriptTextAttributes( 
+		/* [size_is][in] */ __RPC__in_ecount_full(uNumCodeChars) LPCOLESTR pstrCode,
+		/* [in] */ ULONG uNumCodeChars,
+		/* [in] */ __RPC__in LPCOLESTR pstrDelimiter,
+		/* [in] */ DWORD dwFlags,
+		/* [size_is][out][in] */ __RPC__inout_ecount_full(uNumCodeChars) SOURCE_TEXT_ATTR *pattr){
+		
+		if (!m_ActiveScriptDebug)		
+			return E_NOTIMPL;
+
+		return m_ActiveScriptDebug->GetScriptTextAttributes(pstrCode, uNumCodeChars, pstrDelimiter, dwFlags, pattr);
+	}
+
+	virtual HRESULT STDMETHODCALLTYPE OnCreateDocumentContext(IUnknown **ppunkOuter){
+		return E_NOTIMPL;
+	}
+
+	virtual HRESULT STDMETHODCALLTYPE GetPathName(BSTR *pbstrLongName, BOOL *pfIsOriginalFile){
+		*pfIsOriginalFile = true;
+		*pbstrLongName = SysAllocString(m_FilePath);
+		return S_OK;
+	}
+
+	virtual HRESULT STDMETHODCALLTYPE GetFileName(BSTR *pbstrShortName){
+		*pbstrShortName = SysAllocString(PathFindFileName(m_FilePath)); 
+		return (*pbstrShortName == NULL)? E_FAIL : S_OK;
+	}
+
+	virtual HRESULT STDMETHODCALLTYPE NotifyChanged(){
+		return E_NOTIMPL;
+	}
+};
+
 
 ActiveScriptSiteDebug::ActiveScriptSiteDebug(TCHAR *appName, IActiveScript* as):m_ActiveScript(as){
 
 	if (FAILED(CoCreateInstance(
 		CLSID_ProcessDebugManager, NULL,
-		CLSCTX_INPROC_SERVER | CLSCTX_INPROC_HANDLER | CLSCTX_LOCAL_SERVER,
+		CLSCTX_INPROC_SERVER | /*CLSCTX_INPROC_HANDLER |*/ CLSCTX_LOCAL_SERVER,
         __uuidof(IProcessDebugManager), (void **)m_Pdm)))
 		throw TEXT("PDM creation failed");
 
@@ -33,8 +93,6 @@ ActiveScriptSiteDebug::ActiveScriptSiteDebug(TCHAR *appName, IActiveScript* as):
 
 	if (FAILED(m_Pdm->AddApplication(m_App, &m_AppCookie)))
 		throw TEXT("Debug application adding failed");
-
-
 }
 
 ActiveScriptSiteDebug::~ActiveScriptSiteDebug(){
@@ -66,12 +124,25 @@ DWORD ActiveScriptSiteDebug::AddScript(BSTR script, BSTR name){
 		scriptAttr = TEXT_DOC_ATTR_READONLY;
 	}
 
-	HRESULT res = doc->Init(m_App, name, name, scriptAttr);
+	HRESULT res;
+
+	res = doc->Init(m_App, name, name, scriptAttr);
 	res = doc->Attach(NULL);
+
+	res = doc->SetDocumentAttr(scriptAttr);
+
+	// Syntax highlighting via script engine and right file names
+	LocRef<IActiveScriptDebug> asd;
+	if ((res = m_ActiveScript->QueryInterface(__uuidof(IActiveScriptDebug), (void**)asd))==S_OK){
+		LocRef<DebugDocumentHostImpl> ddh(new DebugDocumentHostImpl(name, asd));
+		res = doc->SetDebugDocumentHost(ddh);
+	}
+
 	res = doc->AddUnicodeText(script);
 
 	DWORD srcContext = 0;
 	res = doc->DefineScriptBlock(0, SysStringLen(script), m_ActiveScript, false, &srcContext);
+
 	m_Docs.insert(Docs::value_type(srcContext, doc));
 
 	return srcContext;
@@ -124,3 +195,16 @@ HRESULT ActiveScriptSiteDebug::OnScriptErrorDebug(IActiveScriptErrorDebug *pErro
 	*pfCallOnScriptErrorWhenContinuing=TRUE;
 	return S_OK; 
 }
+
+HRESULT ActiveScriptSiteDebug::QueryInterface(REFIID riid, LPVOID *ppv){
+	if (S_OK == CComBase<IActiveScriptSiteDebug>::QueryInterface(riid, ppv))
+		return S_OK;
+
+	
+	if (IsEqualIID(riid, __uuidof(IActiveScriptSiteDebugEx))){
+		LocRef<ActiveScriptSiteDebugExImpl> debEx(new ActiveScriptSiteDebugExImpl());
+		return debEx->QueryInterface(riid, ppv);
+	}
+
+	return E_NOINTERFACE;
+} 
