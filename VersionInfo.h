93b9ffb62cbed5d98a7e3ffcb3ddde4f238ce5e0
sieukrem <eugen.kremer@gmail.com>
2017-12-12 20:26:15 +0100

    Update (c)

--- VersionInfo.h
+++ VersionInfo.h
@@ -1,6 +1,6 @@
 /*
 This file is part of jN, a plugin for Notepad++
-Copyright (C)2013 Eugen Kremer <eugen DOT kremer AT gmail DOT com>
+Copyright (C)2017 Eugen Kremer <eugen DOT kremer AT gmail DOT com>
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
