80da72e5abaef55b1c2096d5d867e630038d057c
sieukrem <eugen.kremer@gmail.com>
2018-01-03 00:55:20 +0100

    Fixed compile errors for v120 platform tool set

--- common/ActiveScriptSiteDebug.cpp
+++ common/ActiveScriptSiteDebug.cpp
@@ -16,7 +16,6 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 #include "ActiveScriptSiteDebug.h"
-#include <strsafe.h>
 #include <shlwapi.h>
 #include "SysStr.h"
 
--- common/SysStr.h
+++ common/SysStr.h
@@ -18,6 +18,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #pragma once
 
 #include "oleauto.h"
+#include <tchar.h>
 #include <strsafe.h>
 
 class SysStr{
