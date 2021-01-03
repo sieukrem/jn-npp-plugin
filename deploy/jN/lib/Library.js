e41fdcc8aeeb9206e184d1a48ba8c1799ff1f68f
Eugen Kremer <eugen.kremer@siemens.com>
2021-01-03 20:03:38 +0100

    Use CallFrame in javascript

--- deploy/jN/lib/Library.js
+++ deploy/jN/lib/Library.js
@@ -1,33 +1,35 @@
 function Library(path){
 	this.NativeLibrary = System.loadLibrary(path);
 }
+
+var nativeSize = System.isX64()? "pushLWORD" : "pushDWORD";
+/*
+   In case of x64 system it is important to respect parameter passing through register
+   There are integer and floating point register, hence we use appropriate serializer function
+*/
+
 Library.prototype.Types = {
-	"HWND"  :4,
-	"UINT"  :4,
-	"WPARAM":4,
-	"LPARAM":4,
-	"BYTE"  :1,
-	"WORD"  :2,
-	"DWORD" :4,
-	"BOOL"  :4,
-	"LPCTSTR" : 4,
-	"LPCSTR"  : 4, 
-	"LPSTR"   : 4,
-	"LPCWSTR" : 4,
-	"LONG_PTR": 4,
-	"int"     : 4,
-	"POINT"	  : 8,
-	"LPPOINT" : 4,
-	"LPBOOL"  : 4,
-	SizeToSimple:function(size){
-		switch(size){
-			case 1:return "Byte";
-			case 2:return "Word";
-			case 4:return "DWord";
-			case 8:return "Int64";
-		}
-		throw "Unknown size "+size;
-	}
+	"HWND"    : nativeSize,
+	"UINT"    : "pushDWORD",
+	"WPARAM"  : nativeSize,
+	"LPARAM"  : "pushPtr",
+	"BYTE"    : "pushByte",
+	"WORD"    : "pushWord",
+	"DWORD"   : "pushDWORD",
+	"LWORD"   : "pushLWORD",
+	"BOOL"    : "pushDWORD",
+	"LPCTSTR" : "pushPtr",
+	"LPCSTR"  : "pushPtr", 
+	"LPSTR"   : "pushPtr",
+	"LPWSTR"  : "pushPtr",
+	"LPCWSTR" : "pushPtr",
+	"LONG_PTR": "pushPtr",
+	"int"     : "pushDWORD",
+	"POINT"	  : "pushLWORD",
+	"LPPOINT" : "pushPtr",
+	"LPBOOL"  : "pushPtr",
+	"FLOAT"   : "pushFloat",
+	"DOUBLE"  : "pushDouble"
 };
 
 Library.prototype.Define = function(){ // name, params types...
@@ -38,26 +40,22 @@ Library.prototype.Define = function(){ // name, params types...
 		throw "Function definition exists for "+name;
 	
 	// check types
-	var stackSize = 0;
 	for(var i=0,c=types.length; i<c; i++){
 		if (!this.Types[types[i]])
 			throw "Unknown type "+types[i];
-		
-		stackSize += this.Types[types[i]];
 	}
 	
 	this[name] = function(){
 		if (arguments.length != types.length)
 			throw "Expected count of arguments is " + types.length+" but found "+arguments.length;
 			
-		var stack = this.NativeLibrary.alloc(stackSize);
-		var pos = 0;
+		var callFrame = System.createCallFrame();
+		
 		for(var i=0, c=arguments.length; i<c; i++){
-			var size = this.Types[types[i]];
-			this.NativeLibrary["write"+this.Types.SizeToSimple(size)](stack, pos, arguments[i]);
-			pos+=size;
+			var serializerFunction = this.Types[types[i]];
+			callFrame[serializerFunction](arguments[i]);
 		}
 		
-		return this.NativeLibrary.call(name, stack);
+		return this.NativeLibrary.callWithFrame(name, callFrame);
 	}
 }
--- deploy/jN/lib/User32.dll.js
+++ deploy/jN/lib/User32.dll.js
@@ -3,12 +3,12 @@ require("Library.js");
 User32 = new Library("User32.dll");
 
 User32.Define("SendMessageW", "HWND", "UINT", "WPARAM", "LPARAM");
-User32.Define("MessageBoxW", "HWND", "LPCTSTR", "LPCTSTR", "UINT");
+User32.Define("MessageBoxW", "HWND", "LPCWSTR", "LPCWSTR", "UINT");
 User32.Define("SetParent", "HWND", "HWND");
 User32.Define("GetWindowLongW", "HWND", "int");
 User32.Define("SetWindowLongW", "HWND", "int", "LONG_PTR");
 User32.Define("SetWindowPos", "HWND", "HWND", "int","int","int","int","UINT");
-User32.Define("FindWindowExW", "HWND", "HWND", "LPCTSTR", "LPCTSTR");
-User32.Define("GetWindowTextW", "HWND", "LPCTSTR", "int");
-User32.Define("SetWindowTextW", "HWND", "LPCTSTR");
+User32.Define("FindWindowExW", "HWND", "HWND", "LPCWSTR", "LPCWSTR");
+User32.Define("GetWindowTextW", "HWND", "LPWSTR", "int");
+User32.Define("SetWindowTextW", "HWND", "LPCWSTR");
 User32.Define("GetWindowTextLengthW", "HWND");  
